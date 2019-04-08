6e4c9f31cf8b988cbc736ea01f3eea39d26bc662
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-08 19:40:02 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -46,7 +46,7 @@ def turn(board)
   puts "Please enter 1-9:"
   i = gets.chomp
   if valid_move?(board, input_to_index(i)) == true
-    move(board, input_to_index(i), player= "X")
+    move
     puts board
   end
   
