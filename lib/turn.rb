dcfb00e258df5c378a9f2fd5ec808ba3f1593cce
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-08 19:39:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -48,10 +48,7 @@ def turn(board)
   if valid_move?(board, input_to_index(i)) == true
     move(board, input_to_index(i), player= "X")
     puts board
-  else
-    turn(board)  
   end
-    
   
 end
 
